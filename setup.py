d583305a20413ab72a0f7c741e0e925041522720
Antonia <wolf_antonia@gmx.de>
2025-05-14 16:50:36 +0200

    Changes

--- setup.py
+++ setup.py
@@ -1,29 +1,18 @@
 from setuptools import setup, find_packages
 
-package_name = 'mecharm_skills_ros2'  # muss exakt mit <name> in package.xml übereinstimmen
+package_name = 'mecharm_skills_ros2'
 
 setup(
     name=package_name,
     version='0.1.0',
-    packages=find_packages('scripts'),     # durchsucht scripts/ nach Packages
-    package_dir={'': 'scripts'},           # maps package root → scripts/
-
-    install_requires=[
-        'setuptools',
-        'paho-mqtt',
-        'pymycobot',
-    ],
-
-    author='Antonia Wolf',
-    author_email='wolf_antonia@gmx.de',
-    description='ROS2-Package zur Orchestrierung von MechArm-Skills und MQTT-Client',
-    license='MIT',
-
+    packages=find_packages('scripts'),
+    package_dir={'': 'scripts'},
+    install_requires=['setuptools', 'paho-mqtt', 'pymycobot'],
     entry_points={
         'console_scripts': [
-            'mqtt_client    = mqtt_client:main',
-            'main_node      = main_node:main',
-            'fixed_move_to_object = skills.fixed_move_to_object:main'
+            'mqtt_client = mqtt_client:main',
+            'main_node   = main_node:main',
+            'fixed_move_to_object = mecharm_skills.skills.fixed_move_to_object:main',
         ],
     },
 )
